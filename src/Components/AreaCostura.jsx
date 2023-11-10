55942d6f7c42b97a3141770c28795c4743a74e4a
MRGuilherme1 <141660061+MRGuilherme1@users.noreply.github.com>
2023-11-10 14:14:42 -0300

    Primeiro component da homepage, dashboard.

---
+++ src/Components/AreaCostura.jsx
@@ -0,0 +1,18 @@
+import style from '../Module/AreaCostura.module.css'
+import SouCostureiro from '../IMG/SouCostureiro.png.png'
+import SouCliente from '../IMG/SouCliente.png.png'
+function AreaCostura() {
+    return (
+        <div className={style.Conteiner1}>
+            <div className={style.ConteinerMenor1}>
+                <button>Sou costureiro</button>
+            </div>
+            <div className={style.ConteinerMenor2}>
+                <button>Sou costureiro</button>
+            </div>
+
+
+        </div>
+    )
+}
+export default AreaCostura
