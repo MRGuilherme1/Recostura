0f7fc5096ab4e5277f5b47b95ea162c568d72a53
MRGuilherme1 <141660061+MRGuilherme1@users.noreply.github.com>
2023-11-14 02:48:33 -0300

    colocando o carousel sem as routes pq nao sei por ainda

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import AreaCostura from './Components/AreaCostura';
+import CarouselItem from './Components/Carousel';
+
 function App() {
   return (
     <body>
       <AreaCostura />
+      <CarouselItem />
 
 
 
