d7aea8255ede8bf0b01a4d3d2e6b7d1ec10909ac
MRGuilherme1 <141660061+MRGuilherme1@users.noreply.github.com>
2023-11-14 03:35:00 -0300

    to dando mais um commit hein

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import AreaCostura from './Components/AreaCostura';
 import CarouselItem from './Components/Carousel';
+import TextoIntrodutivo from './Components/TextoIntrodutivo';
 
 function App() {
   return (
     <body>
       <AreaCostura />
       <CarouselItem />
-
+      <TextoIntrodutivo />
 
 
 
---
+++ src/Components/TextoIntrodutivo.jsx
@@ -0,0 +1,10 @@
+import style from '../Module/TextoIntrodutivo.Module.css';
+
+function TextoIntrodutivo(){
+    return (
+        <section className={style.TextoIntrodutivo}>
+        <p>Devemos diminuir esse texto.</p>
+        </section>
+    )
+}
+export default TextoIntrodutivo;
