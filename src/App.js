0243a0ec59ac71cf07f2f241191d509d485d26d5
MRGuilherme1 <141660061+MRGuilherme1@users.noreply.github.com>
2023-11-22 17:04:11 -0300

    carrosel pronto

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import AreaCostura from './Components/AreaCostura';
 import TextoIntrodutivo from './Components/TextoIntrodutivo';
 import AreaDoacao from './Components/AreaDoacao';
 import Caarousel from './Components/Carousel';
-import Cabeçalho from './Components/Header';
 function App() {
   return (
     <body>
-      <Cabeçalho />
       <AreaCostura />
       <TextoIntrodutivo />
       <Caarousel />
